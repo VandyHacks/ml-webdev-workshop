b3ef91c4dcd459f3ab44c54e53e72902e39cce56
kyleburgess <kylie.m.burgess@vanderbilt.edu>
2023-04-16 20:52:47 -0500

    Changed comments

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Textbook Reader and Analyzer
+
 This simple React WebApp will analyze images of textbooks and provide the text it contains as well as a summary of its content.
+
 # How To Set Up
+
 After cloning the repository, run `npm i` to download the necessary packages. Then, create a `.env` file in the root folder with the variable `REACT_APP_HUGGINGFACE_TOKEN` set to your HuggingFace token. Run `npm start` to deploy locally.
 
 # Branch Layout
+
 The main branch will contain the bare minimum code that we will be teaching during the series. The `fancy` branch will contian a tricked-out version of the code with all the bells and whistles: pretty CSS (complete with squircle buttons), an option to paste the image rather than choose a file, and perhaps a way to take a pdf and do the analysis on that (even if it has multiple pages ;) ). We will be updating this over time.
--- src/App.js
+++ src/App.js
@@ -1,55 +1,35 @@
-
-import { useState, useEffect} from 'react';
-import './App.css';
-
-// change class to className
-// Add state
-// Display state in the UI
-// Add event handlers
-// Add event handlers to the UI
-// CHALLENGE: Make them write the function to handle click
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  // Kyle stuff (hooks):
   const [imagePath, setImagePath] = useState("");
   const [text, setText] = useState("");
   const [summary, setSummary] = useState("");
- 
-  // kyle stuff (functions):
+
   const handleChange = (event) => {
-    if (event.target.files && event.target.files[0] && event.target.files[0].type.includes("image")){
-      setImagePath(URL.createObjectURL(event.target.files[0]));
-    }
+    setImagePath(URL.createObjectURL(event.target.files[0]));
   };
- 
-  // Kyle stuff (hooks )
+
   const handleClick = async () => {
     // Do the OCR magic here
     setText("Example text");
     setSummary("Example summary");
   };
- 
-  // All of below is Rachael stuff unless otherwise noted
-  // Kyle will talk about/add the onChange and onClick functions
+
   return (
     <div className="App">
       <main className="App-main">
         <h2>Image Summarizer</h2>
         <h3>Choose a file to upload.</h3>
         <input type="file" onChange={handleChange} />
-        {/* { imagePath && <>
+        { imagePath && <>
           <h4>Selected Image</h4>
           <img src={imagePath} className="App-image" alt="logo" />
           <br />
           <button onClick={handleClick}>Convert to text</button>
-        </> } */}
-          <h4>Selected Image</h4>
-          <img src="light.jpg" className="App-image" alt="logo" />
-          <br />
-          <button >Convert to text</button>
-       
+        </> }
 
-        {/* { text && <><h4>Extracted text</h4>
+        { text && <><h4>Extracted text</h4>
         <div className="text-box">
           <p>{ text }</p>
         </div></> } 
@@ -57,23 +37,18 @@ function App() {
         { summary && <><h4>Summary text</h4>
         <div className="text-box">
           <p>{ summary }</p>
-        </div></> }*/}
+        </div></> }
 
-        {/* Instead of the above, do: */}
-        <h4>Extracted text</h4>
-        <div className="text-box">
-          <p>Filler</p>
-        </div> 
-        
         <h4>Summary text</h4>
         <div className="text-box">
           <p>Summary</p>
         </div>
-        
-        <br /><br />
+
+        <br />
+        <br />
       </main>
     </div>
   );
 }
- 
+
 export default App;
